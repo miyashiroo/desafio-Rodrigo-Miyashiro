dfef8af0838c3ab105c61f3de0d11b458de5b554
miyashiro <miyashiroo@gmail.com>
2023-08-14 17:07:02 -0400

    Colocando os itens em array

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,9 +1,28 @@
 class CaixaDaLanchonete {
 
     calcularValorDaCompra(metodoDePagamento, itens) {
+        let tipoPagamento = metodoDePagamento;
+
+        let arrayItens = itens.split(',')
+        console.log(arrayItens)
+        console.log(tipoPagamento)
+
+
+
+
         return "";
     }
 
+
+
+
+
+
+
+
 }
+
+
+
 
 export { CaixaDaLanchonete };
