b3b92d2b093472ac03b988659dc39dfb37d36461
miyashiro <miyashiroo@gmail.com>
2023-08-16 11:35:55 -0400

    Refatoração com try/catch

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,83 +1,59 @@
-import { temQueijoOuChantily,verificaQuantidadeInvalida,verificaCodigoInvalido } from "./validacoes.js";
-import { precoItem  } from "./bancodedados/tabelaDePreco.js";
-
-
+import { verificaQueijoChantily,verificaQuantidadeInvalida,verificaSeCarrinhoVazinho,calculaPagamento,verificaPreco } from "./validacoes.js";
 
 class CaixaDaLanchonete {
-    
     
     calcularValorDaCompra(metodoDePagamento, itens) {
 
         let tipoPagamento = String(metodoDePagamento); 
-        let quantidadeInvalida = false
         let arrayItens = []
         let somaTotal = 0
-        let codigoInvalido = false
-        let validaQueijoEChantily;
+
     
         for (const item of itens) {
             let array = item.split(",")
             let nomeDoItem = array[0]
             let quantidade = array[1]
-            quantidadeInvalida =  verificaQuantidadeInvalida(quantidade)
-    
+
+            try {
+                verificaQuantidadeInvalida(quantidade)
+            } catch (error) {
+                return error.message
+            }
             arrayItens.push({ nomeDoItem, quantidade })
         }
 
-        // verifica se a quantidade que foi passada é 0
-
-        if (quantidadeInvalida) {
-            return "Quantidade inválida!"
+        try {
+            verificaSeCarrinhoVazinho(arrayItens)
+        } catch (error) {
+            return error.message
         }
-
-        // Verificando se o carrinho está vazio
-
-        if (arrayItens.length === 0) {
-            return "Não há itens no carrinho de compra!"
-        }
-
- 
-        // verifica se tem queijo ou chantily e se tiver valida se tem o prato principal
-
-        validaQueijoEChantily = temQueijoOuChantily(arrayItens)
-
-        if (!validaQueijoEChantily) {
-             return "Item extra não pode ser pedido sem o principal"
-        }
-    
         
+
+        try {
+            verificaQueijoChantily(arrayItens)
+        } catch (error) {
+            return error.message
+        }
+
         for (const itemCarrinho of arrayItens) {
 
-            // verifica se o codigo passado é válido.
-            
-            codigoInvalido = verificaCodigoInvalido(itemCarrinho.nomeDoItem);
-
-
-            somaTotal += precoItem(itemCarrinho.nomeDoItem) * Number(itemCarrinho.quantidade)
+            try {
+                somaTotal += verificaPreco(itemCarrinho.nomeDoItem) * Number(itemCarrinho.quantidade)
+            } catch (error) {
+                return error.message
+            }
+    
         
         }
 
-        // se for invalido o programa encerra
-        if (codigoInvalido) {
-            return "Item inválido!"
-        }
-
-        // Aplicando Descontos e Taxas
-
-        if (tipoPagamento === 'dinheiro') {
-            somaTotal = somaTotal * 0.95
-        
-        } else if( tipoPagamento === "credito") {
-            somaTotal = somaTotal * 1.03
-
-        } else if(tipoPagamento === "debito") {
-            somaTotal = somaTotal * 1
-        } else {
-            return "Forma de pagamento inválida!"
+        try {
+            somaTotal = calculaPagamento(tipoPagamento,somaTotal)
+        } catch (error) {
+            return error.message
         }
 
     
-        let totalFormatado = somaTotal.toFixed(2).replace('.',',')
+        const totalFormatado = somaTotal.toFixed(2).replace('.',',')
     
         return `R$ ${totalFormatado}`;
     }
--- src/validacoes.js
+++ src/validacoes.js
@@ -1,60 +1,91 @@
-import { precoItem } from "./bancodedados/tabelaDePreco"
 
-const temQueijoOuChantily = (array) => {
-    let resposta = true
+const verificaQueijoChantily = (array) => {
+ 
 
-    let ehChantily = array.find((item) => {
+    let temChantily = array.find((item) => {
         return item.nomeDoItem === 'chantily'
     })
 
-    let ehQueijo = array.find((item) => {
+    let temQueijo = array.find((item) => {
         return item.nomeDoItem === "queijo"
     })
 
-    if (ehChantily) {
+    if (temChantily) {
         let temCafe = array.find((item) => { return item.nomeDoItem === "cafe" })
         if (temCafe) {
-            return resposta
+            return ;
         } else {
-            resposta = false
+            throw new Error("Item extra não pode ser pedido sem o principal")     
         }
         
     }
 
-    if (ehQueijo) {
+    if (temQueijo) {
         let temSanduiche = array.find((item) => { return item.nomeDoItem === "sanduiche" })
         if (temSanduiche) {
-            return resposta
+            return ;
         } else {
-            resposta = false
-        }
+            throw new Error("Item extra não pode ser pedido sem o principal")        }
  
     }
-
-
-
-    return resposta
-
-
+    return ;
 }
 
 const verificaQuantidadeInvalida = (valor) => {
-    if (Number(valor) === 0) {
-        return true
-    } else {
-        return false
-    }
-}
-
-const verificaCodigoInvalido = (codigo) => {
-    if (precoItem(codigo) === -1) {
-        return true;
- 
-    } else {
-        return false
+    if(Number(valor) === 0 ) {
+        throw new Error("Quantidade inválida!")
     }
 }
 
 
+const verificaSeCarrinhoVazinho = (arrayItens) => {
 
-export {temQueijoOuChantily, verificaQuantidadeInvalida,verificaCodigoInvalido}
+    if (arrayItens.length === 0) {
+        throw new Error("Não há itens no carrinho de compra!");  
+    }
+}
+
+const calculaPagamento = (tipoPagamento,somaTotal) => {
+  
+    if (tipoPagamento === 'dinheiro') {
+       return somaTotal * 0.95
+    
+    } else if ( tipoPagamento === "credito") {
+        return somaTotal * 1.03
+
+    } else if (tipoPagamento === "debito") {
+        return  somaTotal * 1
+
+    } else {
+        throw new Error("Forma de pagamento inválida!")
+    }
+}
+
+const verificaPreco = (item) => {
+
+    switch (item) {
+        case "cafe":
+            return 3.0;
+        case "chantily":
+            return 1.5
+        case "suco":
+            return 6.2
+        case "sanduiche":
+            return 6.5
+        case "queijo":
+            return 2;
+        case "salgado":
+            return 7.25
+        case "combo1":
+            return 9.5
+        case "combo2":
+            return 7.5
+        default:
+             throw new Error("Item inválido!")
+    }
+
+
+
+}
+
+export {verificaQueijoChantily, verificaQuantidadeInvalida, verificaSeCarrinhoVazinho, calculaPagamento,verificaPreco}
